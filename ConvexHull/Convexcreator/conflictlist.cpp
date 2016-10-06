67ec8ee2424763e5bd25e6f05599ae9404368bdc
simociccio <simociccio2@hotmail.it>
2016-10-06 18:07:19 +0200

    adding conflictlist

---
+++ ConvexHull/Convexcreator/conflictlist.cpp
@@ -0,0 +1,6 @@
+#include "conflictlist.h"
+
+conflictlist::conflictlist()
+{
+
+}
---
+++ ConvexHull/Convexcreator/conflictlist.h
@@ -0,0 +1,11 @@
+#ifndef CONFLICTLIST_H
+#define CONFLICTLIST_H
+
+
+class conflictlist
+{
+public:
+    conflictlist();
+};
+
+#endif // CONFLICTLIST_H
